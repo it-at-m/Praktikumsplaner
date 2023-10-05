cb4eb2c008cdb746419026239639bfa875f381d4
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-05 10:35:28 +0200

    ✨ added Frontend Feedback and better Validation

--- praktikumsplaner-frontend/frontend/src/components/ExcelImportNWK.vue
+++ praktikumsplaner-frontend/frontend/src/components/ExcelImportNWK.vue
@@ -11,50 +11,66 @@
                 Import NWK
             </v-btn>
         </template>
-        <v-card>
-            <v-card-title>Excel Datei mit NWKs Hochladen</v-card-title>
-            <v-list>
-                <v-list-item>
-                    <v-file-input
-                        v-model="excelDatei"
-                        accept=".xlsx"
-                        label="Fügen Sie eine Excel Datei ein"
+        <v-form ref="form">
+            <v-card>
+                <v-card-title>Excel Datei mit NWKs Hochladen</v-card-title>
+                <v-list>
+                    <v-list-item>
+                        <v-file-input
+                            v-model="excelDatei"
+                            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                            :rules="fileRules"
+                            label="Fügen Sie eine Excel Datei ein"
+                        >
+                        </v-file-input>
+                    </v-list-item>
+                </v-list>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                        text
+                        @click="cancel()"
                     >
-                    </v-file-input>
-                </v-list-item>
-            </v-list>
-            <v-card-actions>
-                <v-spacer />
-                <v-btn
-                    text
-                    @click="cancel()"
-                >
-                    Abbrechen
-                </v-btn>
-                <v-btn
-                    color="primary"
-                    @click="uploadFile()"
-                >
-                    Hochladen
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+                        Abbrechen
+                    </v-btn>
+                    <v-btn
+                        color="primary"
+                        @click="uploadFile()"
+                    >
+                        Hochladen
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-form>
     </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useSnackbarStore } from "@/stores/snackbar";
+import { Levels } from "@/api/error";
 import ExcelService from "@/api/ExcelService";
 
 const visible = ref<boolean>();
 const excelDatei = ref<File>();
+const form = ref<HTMLFormElement>();
+const snackbarStore = useSnackbarStore();
+
+const fileRules = [
+    (value: File) =>
+        !value ||
+        value.type ==
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+        "Nur Excel Dateien sind Erlaubt",
+];
 
 function cancel() {
     visible.value = false;
+    form.value?.reset();
 }
 
 function uploadFile() {
-    if (!excelDatei.value) return;
+    if (!excelDatei.value || !form.value?.validate()) return;
 
     // File Reader encodes as Base64
     const reader = new FileReader();
@@ -62,7 +78,22 @@ function uploadFile() {
     reader.onloadend = () => {
         if (typeof reader.result == "string") {
             // Base64 String starts after the comma
-            ExcelService.uploadExcelFile(reader.result.split(",")[1]);
+            ExcelService.uploadExcelFile(reader.result.split(",")[1])
+                .then(() =>
+                    snackbarStore.showMessage({
+                        message: "NWKs erfolgreich Angelegt!",
+                        level: Levels.INFO,
+                    })
+                )
+                .catch((error) => {
+                    snackbarStore.showMessage({
+                        message: error,
+                        level: Levels.ERROR,
+                    });
+                })
+                .finally(() => {
+                    cancel();
+                });
         }
     };
 }
