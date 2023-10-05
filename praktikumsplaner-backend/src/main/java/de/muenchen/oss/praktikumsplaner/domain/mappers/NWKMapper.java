c8e2625c31abaa481cb2af26d706f0993c18bca2
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-05 09:42:35 +0200

    ✨ added NWK Mapper and Repo

---
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/mappers/NWKMapper.java
@@ -0,0 +1,10 @@
+package de.muenchen.oss.praktikumsplaner.domain.mappers;
+
+import de.muenchen.oss.praktikumsplaner.domain.NWK;
+import de.muenchen.oss.praktikumsplaner.domain.dtos.NwkDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface NWKMapper {
+    NWK toEntity(NwkDTO nwkDTO);
+}
