5b71f8821d426bf40ef4781a71f1f7f0299c4dc3
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-10-10 16:53:27 +0200

    🦺 custom validator assuring, a date is before another date

---
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/annotations/StartDateBeforeEndDate.java
@@ -0,0 +1,29 @@
+package de.muenchen.oss.praktikumsplaner.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import de.muenchen.oss.praktikumsplaner.validators.StartDateBeforeEndDateValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = { StartDateBeforeEndDateValidator.class })
+@Documented
+public @interface StartDateBeforeEndDate {
+    String message() default "{constraints.validDates}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String startDate();
+
+    String endDate();
+}
---
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/validators/StartDateBeforeEndDateValidator.java
@@ -0,0 +1,39 @@
+package de.muenchen.oss.praktikumsplaner.validators;
+
+import de.muenchen.oss.praktikumsplaner.annotations.StartDateBeforeEndDate;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.reflect.Method;
+import java.time.LocalDate;
+import org.apache.commons.beanutils.BeanUtils;
+
+public class StartDateBeforeEndDateValidator implements ConstraintValidator<StartDateBeforeEndDate, Object> {
+    private String startDateFieldName;
+    private String endDateFieldName;
+
+    @Override
+    public void initialize(final StartDateBeforeEndDate constraintAnnotation) {
+        startDateFieldName = constraintAnnotation.startDate();
+        endDateFieldName = constraintAnnotation.endDate();
+    }
+
+    @Override
+    public boolean isValid(final Object o, final ConstraintValidatorContext constraintValidatorContext) {
+        try {
+            if (o == null) {
+                return true;
+            }
+
+            final Method startDateGetter = o.getClass().getMethod(startDateFieldName);
+            final LocalDate startDate = (LocalDate) startDateGetter.invoke(o);
+
+            final Method endDateGetter = o.getClass().getMethod(endDateFieldName);
+            final LocalDate endDate = (LocalDate) endDateGetter.invoke(o);
+
+            return startDate.isBefore(endDate);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
