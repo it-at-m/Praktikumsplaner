7e1a3b6fc51b5af52ca45c5e1d18c6d6b46ad2a1
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-13 11:59:37 +0200

    :art: made vorlesungstage Enum and removed columns

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/CreateNwkDTO.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/CreateNwkDTO.java
@@ -4,13 +4,15 @@ import de.muenchen.oss.praktikumsplaner.domain.Studiengang;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.time.DayOfWeek;
+import java.util.Set;
 import lombok.Builder;
 
 @Builder
 public record CreateNwkDTO(
-                    @NotNull(message = "Der Vorname ist erforderlich") @Size(min = 2, max = 255, message = "Der Vorname darf nur zwischen {min} und {max} Zeichen lang sein") String vorname,
-                     @NotNull(message = "Der Nachname ist erforderlich") @Size(min = 2, max = 255, message = "Der Nachname darf nur zwischen {min} und {max} Zeichen lang sein") String nachname,
-                     @NotNull(message = "Der Studiengang ist erforderlich") Studiengang studiengang,
-                     @NotNull(message = "Der Jahrgang ist erforderlich") @Pattern(regexp = "\\d\\d/\\d\\d") String jahrgang,
-                     @Pattern(regexp = "(Mo|Di|Mi|Do|Fr|Sa)( \\+ (Mo|Di|Mi|Do|Fr|Sa)){0,5}") String vorlesungstage) {
+        @NotNull(message = "Der Vorname ist erforderlich") @Size(min = 2, max = 255, message = "Der Vorname darf nur zwischen {min} und {max} Zeichen lang sein") String vorname,
+        @NotNull(message = "Der Nachname ist erforderlich") @Size(min = 2, max = 255, message = "Der Nachname darf nur zwischen {min} und {max} Zeichen lang sein") String nachname,
+        @NotNull(message = "Der Studiengang ist erforderlich") Studiengang studiengang,
+        @NotNull(message = "Der Jahrgang ist erforderlich") @Pattern(regexp = "\\d\\d/\\d\\d") String jahrgang,
+        Set<DayOfWeek> vorlesungstage) {
 }
--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/NwkDTO.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/NwkDTO.java
@@ -4,6 +4,8 @@ import de.muenchen.oss.praktikumsplaner.domain.Studiengang;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.time.DayOfWeek;
+import java.util.Set;
 import java.util.UUID;
 import lombok.Builder;
 
@@ -13,5 +15,5 @@ public record NwkDTO(UUID id,
                      @NotNull(message = "Der Nachname ist erforderlich") @Size(min = 2, max = 255, message = "Der Nachname darf nur zwischen {min} und {max} Zeichen lang sein") String nachname,
                      @NotNull(message = "Der Studiengang ist erforderlich") Studiengang studiengang,
                      @NotNull(message = "Der Jahrgang ist erforderlich") @Pattern(regexp = "\\d\\d/\\d\\d") String jahrgang,
-                     @Pattern(regexp = "(Mo|Di|Mi|Do|Fr|Sa)( \\+ (Mo|Di|Mi|Do|Fr|Sa)){0,5}") String vorlesungstage) {
+                     Set<DayOfWeek> vorlesungstage) {
 }
