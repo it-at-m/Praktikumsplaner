350f1297d7d99651fccb9d36200fa5331557d13d
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-13 13:56:59 +0200

    🚚 renamed fileFormatRules to fileTypeRules

--- praktikumsplaner-frontend/frontend/src/composables/rules.ts
+++ praktikumsplaner-frontend/frontend/src/composables/rules.ts
@@ -1,7 +1,7 @@
 export function useRules() {
-    function fileFormatRule(format: string, message: string) {
+    function fileTypeRule(format: string, message: string) {
         return (value: File) => (value && value.type == format) || message;
     }
 
-    return { fileFormatRule };
+    return { fileTypeRule };
 }
