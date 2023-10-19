bc4bbc21c85196102899abc63b9d270dc8021409
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-10-19 14:15:52 +0200

    :rotating_light: ignore unmapped target property

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/mappers/MeldezeitraumMapper.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/mappers/MeldezeitraumMapper.java
@@ -4,10 +4,12 @@ import de.muenchen.oss.praktikumsplaner.domain.Meldezeitraum;
 import de.muenchen.oss.praktikumsplaner.dtos.MeldezeitraumCreateDTO;
 import de.muenchen.oss.praktikumsplaner.dtos.MeldezeitraumDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface MeldezeitraumMapper {
     MeldezeitraumDTO toDto(Meldezeitraum meldezeitraum);
 
+    @Mapping(target = "id", ignore = true)
     Meldezeitraum toEntity(MeldezeitraumCreateDTO meldezeitraumCreateDTO);
 }
