107ad31473ed97c1b3e6e9f49d93ed4adf66ae0b
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-20 15:19:33 +0200

    :art: fixed imports

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstelleRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstelleRepository.java
@@ -1,8 +1,7 @@
 package de.muenchen.oss.praktikumsplaner.repository;
 
-import java.util.UUID;
-
 import de.muenchen.oss.praktikumsplaner.domain.StudiumsPraktikumsstelle;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
