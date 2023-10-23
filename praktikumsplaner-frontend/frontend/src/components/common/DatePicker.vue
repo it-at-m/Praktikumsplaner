8e860fcf93a43feeb28ebffd2267f686ee2a2ee8
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-10-23 11:20:55 +0200

    :rotating_light: fix type specification to fix build

--- praktikumsplaner-frontend/frontend/src/components/common/DatePicker.vue
+++ praktikumsplaner-frontend/frontend/src/components/common/DatePicker.vue
@@ -24,7 +24,7 @@ import { computed, ref } from "vue";
 const props = defineProps<{
     prependIcon: string;
     label: string;
-    rules: ((value: string) => string | true | undefined)[];
+    rules: ((value: string) => string | boolean )[];
     value?: string;
 }>();
 
