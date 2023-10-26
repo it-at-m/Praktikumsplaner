4b746d00bd72c9d4c760f20c633b297a3145eaba
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-26 11:50:55 +0200

    :art:🦺 added email validation and renamed Repos to fit naming

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AusbildungsPraktikumsstelleRepository extends CrudRepository<AusbildungsPraktikumsstelle, UUID> {
+public interface AusbildungsPraktikumsstellenRepository extends CrudRepository<AusbildungsPraktikumsstelle, UUID> {
 }
--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstellenRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstellenRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudiumsPraktikumsstelleRepository extends CrudRepository<StudiumsPraktikumsstelle, UUID> {
+public interface StudiumsPraktikumsstellenRepository extends CrudRepository<StudiumsPraktikumsstelle, UUID> {
 }
