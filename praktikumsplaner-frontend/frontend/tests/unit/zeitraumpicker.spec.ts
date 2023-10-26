c6e398963a85ac1097f759227d62a9932ffb0d49
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-10-26 15:51:10 +0200

    🚧:white_check_mark: tried to fix build

--- praktikumsplaner-frontend/frontend/tests/unit/zeitraumpicker.spec.ts
+++ praktikumsplaner-frontend/frontend/tests/unit/zeitraumpicker.spec.ts
@@ -1,24 +1,8 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
-import Vuetify from "vuetify";
-import Vue from "vue";
-import { createPinia, PiniaVuePlugin } from "pinia";
+import { shallowMount } from "@vue/test-utils";
 import ZeitraumPicker from "@/components/Meldezeitraeume/ZeitraumPicker.vue";
 import Zeitraum from "@/types/Zeitraum";
 
-const localVue = createLocalVue();
-const pinia = createPinia();
-
 describe("Zeitraumpicker.vue", () => {
-    let vuetify: any;
-
-    beforeAll(() => {
-        Vue.use(PiniaVuePlugin);
-        Vue.use(Vuetify);
-    });
-
-    beforeEach(() => {
-        vuetify = new Vuetify();
-    });
 
     it("renders error messages in Zeitraumpicker when date order is wrong", () => {
         const errorMessageStart = "Das Startdatum muss vor dem Enddatum liegen.";
@@ -26,10 +10,9 @@ describe("Zeitraumpicker.vue", () => {
 
         const zeitraum = new Zeitraum("2020-10-10", "2019-10-10");
         const wrapper = shallowMount(ZeitraumPicker, {
-            localVue,
-            pinia,
-            vuetify,
-            propsData: { value: zeitraum},
+            propsData: {
+                    value: zeitraum
+                },
         });
 
         expect(wrapper.html()).toContain(errorMessageEnd);
@@ -42,9 +25,6 @@ describe("Zeitraumpicker.vue", () => {
 
         const zeitraum = new Zeitraum("2019-10-10", "2020-10-10");
         const wrapper = shallowMount(ZeitraumPicker, {
-            localVue,
-            pinia,
-            vuetify,
             propsData: { value: zeitraum},
         });
 
