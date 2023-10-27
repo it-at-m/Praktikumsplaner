9998370bd4d9665402651e24af4c28b1159dc68b
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-27 12:05:35 +0200

    :art: unified Ausbildungsrichtung.java and Studienart.java to Studienart.java

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/CreateAusbildungsPraktikumsstelleDTO.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/CreateAusbildungsPraktikumsstelleDTO.java
@@ -1,15 +1,17 @@
 package de.muenchen.oss.praktikumsplaner.domain.dtos;
 
+import de.muenchen.oss.praktikumsplaner.annotations.AusbildungsAnnotation;
 import de.muenchen.oss.praktikumsplaner.domain.enums.Ausbildungsjahr;
-import de.muenchen.oss.praktikumsplaner.domain.enums.Ausbildungsrichtung;
 import de.muenchen.oss.praktikumsplaner.domain.enums.Dringlichkeit;
 import de.muenchen.oss.praktikumsplaner.domain.enums.Referat;
+import de.muenchen.oss.praktikumsplaner.domain.enums.Studiengang;
 import de.muenchen.oss.praktikumsplaner.domain.enums.YesNo;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
+@AusbildungsAnnotation(studiengang = "ausbildungsrichtung", message = "Keine korrekte Ausbildungsrichtung")
 public record CreateAusbildungsPraktikumsstelleDTO(@NotNull String dienststelle,
 
                                                    @NotNull String oertlicheAusbiler,
@@ -28,7 +30,7 @@ public record CreateAusbildungsPraktikumsstelleDTO(@NotNull String dienststelle,
 
                                                    @NotNull Ausbildungsjahr ausbildungsjahr,
 
-                                                   @NotNull Ausbildungsrichtung ausbildungsrichtung
+                                                   @NotNull Studiengang ausbildungsrichtung
 
                         ){
 }
