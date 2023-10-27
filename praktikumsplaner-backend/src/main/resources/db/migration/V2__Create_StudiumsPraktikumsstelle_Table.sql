e9a8e0dc4096098b150fb11e13b9ec9d4162c871
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-27 14:45:00 +0200

    🗃️ added flyway scripts

---
+++ praktikumsplaner-backend/src/main/resources/db/migration/V2__Create_StudiumsPraktikumsstelle_Table.sql
@@ -0,0 +1,16 @@
+create table STUDIUMSPRAKTIKUMSSTELLE
+(
+    id                      varchar(36)  not null,
+    dienststelle            varchar(10) not null,
+    oertlicheAusbilder       varchar(255) not null,
+    email                   varchar(255) not null,
+    taetigkeiten            varchar(5000) not null,
+    dringlichkeit           varchar(10) not null,
+    namentlicheAnforderung  varchar(255),
+    referat                 varchar(3),
+    programmierkenntnisse   varchar(4) not null,
+    studiensemester         varchar(8) not null,
+    studienart              varchar(3) not null,
+    primary key (id)
+);
+
