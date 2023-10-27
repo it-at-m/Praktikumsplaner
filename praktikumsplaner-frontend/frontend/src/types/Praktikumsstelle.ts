e9ef5318fa382e9068ed5b65a395024f1961aef0
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-10-27 14:46:41 +0200

    ✏️ fixed Ausbiler -> Ausbilder frontend

--- praktikumsplaner-frontend/frontend/src/types/Praktikumsstelle.ts
+++ praktikumsplaner-frontend/frontend/src/types/Praktikumsstelle.ts
@@ -2,7 +2,7 @@ export default class Praktikumsstelle {
     constructor(
         public dienststelle: string,
 
-        public oertlicheAusbiler: string,
+        public oertlicheAusbilder: string,
 
         public email: string,
 
