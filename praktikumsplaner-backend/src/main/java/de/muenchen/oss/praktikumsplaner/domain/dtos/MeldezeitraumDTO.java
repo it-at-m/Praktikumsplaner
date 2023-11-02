ea5c637fade55565f6ad23e818a0f8efaae2e1d3
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-11-02 11:27:10 +0100

    :safety_vest: added @notblank to match frontend

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDTO.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDTO.java
@@ -1,16 +1,20 @@
 package de.muenchen.oss.praktikumsplaner.dtos;
 
 import de.muenchen.oss.praktikumsplaner.annotations.StartDateBeforeEndDate;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.UUID;
 import lombok.Builder;
 
 @Builder
 @StartDateBeforeEndDate(startDate = "startZeitpunkt", endDate = "endZeitpunkt", message = "Startdatum muss vor dem Enddatum liegen.")
-public record CreateMeldezeitraumDTO(
+public record MeldezeitraumDTO(
+        @NotNull UUID id,
         @NotNull
         @Size(max = 255, message = "Name für Zeitraum darf nicht länger als {max} Zeichen sein.")
+        @NotBlank
         String zeitraumName,
         @NotNull LocalDate startZeitpunkt,
         @NotNull LocalDate endZeitpunkt)
