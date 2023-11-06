0f7ab22e7c56c78e3efb59cb1425c5f4073f602d
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-11-06 08:35:14 +0100

    🔀💄 Design meldezeitraum Implemented (#83)
    * :white_check_mark: updated validator test to only test the specific validator
    
    * 🗃️ correct version number
    
    * :art: rethrow all exceptions in validator as RuntimeExceptions
    
    * :white_check_mark: shorten assert in test
    
    * :safety_vest: added @notblank to match frontend
    
    * 🚚 moved dtos in the respective package
    
    * :art: snackbar message storing is part of service
    
    * :safety_vest: dependent field validation changed
    
    * :white_check_mark: frontend unit test fix
    
    * :art: spotless fix
    
    * :art: unused import
    
    * :art: import order checkstyle
    
    * 💄 implemented design
    
    * 📝 adjusted the documentation to the design
    
    * :lipstick: adjusted button space to page end
    
    * :safety_vest: changed NotBlank to NotNull to allow blank names
    
    * :fire: removed unused imports

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDTO.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDTO.java
@@ -1,7 +1,6 @@
 package de.muenchen.oss.praktikumsplaner.domain.dtos;
 
 import de.muenchen.oss.praktikumsplaner.annotations.StartDateBeforeEndDate;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
@@ -12,9 +11,8 @@ import lombok.Builder;
 @StartDateBeforeEndDate(startDate = "startZeitpunkt", endDate = "endZeitpunkt", message = "Startdatum muss vor dem Enddatum liegen.")
 public record MeldezeitraumDTO(
         @NotNull UUID id,
-        @NotNull
         @Size(max = 255, message = "Name für Zeitraum darf nicht länger als {max} Zeichen sein.")
-        @NotBlank
+        @NotNull
         String zeitraumName,
         @NotNull LocalDate startZeitpunkt,
         @NotNull LocalDate endZeitpunkt)
