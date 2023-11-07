4097005bbebf5a72d1e8a6d0aa8228d08b54bb5a
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-07 11:40:32 +0100

    ♻️ Snack bar message converted into service

--- praktikumsplaner-frontend/frontend/src/api/MeldungService.ts
+++ praktikumsplaner-frontend/frontend/src/api/MeldungService.ts
@@ -1,33 +1,53 @@
 import FetchUtils from "@/api/FetchUtils";
 import { API_BASE, PRAKTIKUMSSTELLE_BASE } from "@/Constants";
 import Praktikumsstelle from "@/types/Praktikumsstelle";
+import { useSnackbarStore } from "@/stores/snackbar";
+import { Levels } from "@/api/error";
 
 export default {
     uploadStudiumsPraktikumsstelle(
         praktikumsstelle: Praktikumsstelle
-    ): Promise<void> {
+    ): Promise<Praktikumsstelle> {
         return fetch(
             `${API_BASE}${PRAKTIKUMSSTELLE_BASE}/studium`,
             FetchUtils.getPOSTConfig(praktikumsstelle)
         )
             .then((response) => {
+                useSnackbarStore().showMessage({
+                    message: "☑ Speichern erfolgreich.",
+                    level: Levels.SUCCESS,
+                });
                 FetchUtils.defaultResponseHandler(response);
+                return response.json();
             })
             .catch((err) => {
+                useSnackbarStore().showMessage({
+                    message: err.message,
+                    level: Levels.ERROR,
+                });
                 FetchUtils.defaultResponseHandler(err);
             });
     },
     uploadAusbildungsPraktikumsstelle(
         praktikumsstelle: Praktikumsstelle
-    ): Promise<void> {
+    ): Promise<Praktikumsstelle> {
         return fetch(
             `${API_BASE}${PRAKTIKUMSSTELLE_BASE}/ausbildung`,
             FetchUtils.getPOSTConfig(praktikumsstelle)
         )
             .then((response) => {
+                useSnackbarStore().showMessage({
+                    message: "☑ Speichern erfolgreich.",
+                    level: Levels.SUCCESS,
+                });
                 FetchUtils.defaultResponseHandler(response);
+                return response.json();
             })
             .catch((err) => {
+                useSnackbarStore().showMessage({
+                    message: err.message,
+                    level: Levels.ERROR,
+                });
                 FetchUtils.defaultResponseHandler(err);
             });
     },
