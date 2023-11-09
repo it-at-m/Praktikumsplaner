aabe7c2fb5de983d406e72aa6f181cf97a8483cf
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-11-09 11:52:16 +0100

    :lock: frontend security

--- praktikumsplaner-frontend/frontend/src/directives/Security.ts
+++ praktikumsplaner-frontend/frontend/src/directives/Security.ts
@@ -1,39 +1,40 @@
-import Vue from "vue";
-
-import { APP_SECURITY } from "@/Constants";
+import Vue, { watch } from "vue";
 import Vuetify from "vuetify/lib";
 import { useUserStore } from "@/stores/user";
-import { createPinia, PiniaVuePlugin } from "pinia";
+import {APP_SECURITY} from "@/Constants";
 
 Vue.use(Vuetify);
-Vue.use(PiniaVuePlugin);
-Vue.use(createPinia);
 
 Vue.directive("security", {
     inserted: function (el, binding, vnode) {
+        if (!APP_SECURITY) return;
         const userStore = useUserStore();
 
-        // eslint-disable-next-line no-console
-        console.log(userStore);
-        // eslint-disable-next-line no-console
-        console.log(userStore.$state);
-        // eslint-disable-next-line no-console
-        console.log(userStore.$state.roles);
-        //binding.value ist die geforderte Rolle
-        if (userStore.$state.roles && APP_SECURITY) {
-            if (Array.isArray(binding.value)) {
-                if (
-                    !binding.value.some((a) =>
-                        userStore.$state.roles?.includes(a)
-                    )
-                ) {
-                    //Wenn der User nicht eine der geforderten Rollen besitzt, dann wird das Element nicht angezeigt.
-                    vnode.elm?.parentElement?.removeChild(vnode.elm);
-                }
-            } else if (!userStore.$state.roles?.includes(binding.value)) {
-                //Wenn der User nicht die geforderte Rolle besitzt, dann wird das Element nicht angezeigt.
-                vnode.elm?.parentElement?.removeChild(vnode.elm);
-            }
+        const requiredRole = binding.value;
+
+        const nodeElement = vnode.elm;
+        if (nodeElement) {
+            (nodeElement as HTMLElement).style.display = "none";
         }
+
+        watch(
+            () => userStore.getRoles,
+            (newRoles) => {
+                if (newRoles.some((role) => role == requiredRole)) {
+                    const nodeElement = vnode.elm;
+                    if (nodeElement) {
+                        (nodeElement as HTMLElement).style.display = "";
+                    }
+                } else {
+                    const nodeElement = vnode.elm;
+                    if (nodeElement) {
+                        (nodeElement as HTMLElement).style.display = "none";
+                    }
+                }
+            },
+            {
+                deep: true,
+            }
+        );
     },
 });
