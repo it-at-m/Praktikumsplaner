d5176ce92b9c26291a1f85437f9c2e8971bdbf85
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-14 15:56:28 +0100

    🚧 added query for repo method

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/NWKRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/NWKRepository.java
@@ -2,10 +2,12 @@ package de.muenchen.oss.praktikumsplaner.repository;
 
 import de.muenchen.oss.praktikumsplaner.domain.NWK;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NWKRepository extends CrudRepository<NWK, UUID> {
+    @Query("select * from NWK where isActive")
     Iterable<NWK> findAllByActiveIsTrue();
 }
