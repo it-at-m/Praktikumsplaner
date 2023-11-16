3b5648fba0c8fa749aadfb0c47eff2fa293adad4
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-16 13:49:35 +0100

    ✨ get nwks

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/NWKService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/NWKService.java
@@ -26,6 +26,6 @@ public class NWKService {
     }
 
     public Iterable<NWK> findAllActiveNWKs() {
-        return nwkRepository.findAllByActiveIsTrue();
+        return nwkRepository.findNWKSByIsActiveIsTrue();
     }
 }
