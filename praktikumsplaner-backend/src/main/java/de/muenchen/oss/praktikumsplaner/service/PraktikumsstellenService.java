e7a1a3f8f3c3ff204bd3d6b3b61c276c22067c22
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-20 12:55:21 +0100

    ✨ getAll Praktikumsstellen sorted by "dienststelle"

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
@@ -1,5 +1,8 @@
 package de.muenchen.oss.praktikumsplaner.service;
 
+import de.muenchen.oss.praktikumsplaner.domain.AusbildungsPraktikumsstelle;
+import de.muenchen.oss.praktikumsplaner.domain.BasePraktikumsstelle;
+import de.muenchen.oss.praktikumsplaner.domain.StudiumsPraktikumsstelle;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.AusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleDTO;
@@ -9,6 +12,10 @@ import de.muenchen.oss.praktikumsplaner.repository.AusbildungsPraktikumsstellenR
 import de.muenchen.oss.praktikumsplaner.repository.StudiumsPraktikumsstellenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -25,5 +32,26 @@ public class PraktikumsstellenService {
     public AusbildungsPraktikumsstelleDTO saveAusbildungsPraktikumsstelle(final CreateAusbildungsPraktikumsstelleDTO createAusbildungsPraktikumsstelleDTO) {
         return praktikumsstellenMapper
                 .toDTO(ausbildungsPraktikumsstellenRepository.save(praktikumsstellenMapper.toEntity(createAusbildungsPraktikumsstelleDTO)));
+    }
+
+    public Iterable<BasePraktikumsstelle> getAllPraktiumsstellen() {
+        Iterable<AusbildungsPraktikumsstelle> ausbildungsIterable = ausbildungsPraktikumsstellenRepository.findAll();
+        Iterable<StudiumsPraktikumsstelle> studiumsIterable = studiumsPraktikumsstellenRepository.findAll();
+
+        List<BasePraktikumsstelle> ausbildungsList = new ArrayList<>();
+        ausbildungsIterable.forEach(ausbildungsList::add);
+
+        List<BasePraktikumsstelle> studiumsList = new ArrayList<>();
+        studiumsIterable.forEach(studiumsList::add);
+
+        List<BasePraktikumsstelle> combinedList = new ArrayList<>();
+        combinedList.addAll(ausbildungsList);
+        combinedList.addAll(studiumsList);
+
+        combinedList.sort(Comparator.comparing(BasePraktikumsstelle::getDienststelle));
+
+        Iterable<BasePraktikumsstelle> sortedIterable = combinedList;
+
+        return sortedIterable;
     }
 }
