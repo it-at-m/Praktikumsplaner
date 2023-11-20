f66954509f2a3dcf6b3aea49db5f485bccef1c79
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-20 14:52:52 +0100

    🏷️ added NWK as type in frontend

---
+++ praktikumsplaner-frontend/frontend/src/types/NWK.ts
@@ -0,0 +1,11 @@
+export default class NWK {
+    constructor(
+        public id: string,
+        public vorname: string,
+        public nachname: string,
+        public studiengang: string,
+        public jahrgang: string,
+        public vorlesungstage: string,
+        public isActive: boolean
+    ) {}
+}
