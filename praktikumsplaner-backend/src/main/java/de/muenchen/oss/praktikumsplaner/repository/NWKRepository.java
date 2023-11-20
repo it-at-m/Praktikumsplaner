0076a9a61da348d004ec6e07421eaa1b445edf54
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-20 12:29:31 +0100

    ✨ NWKs are now sorted by their lastname

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/NWKRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/NWKRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NWKRepository extends CrudRepository<NWK, UUID> {
 
-    Iterable<NWK> findNWKSByIsActiveIsTrue();
+    Iterable<NWK> findNWKSByIsActiveIsTrueOrderByNachname();
 }
