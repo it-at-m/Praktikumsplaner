50ba8212247b79b461a1269569fd52c21545ea95
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-20 13:13:09 +0100

    🧑‍💻 GetRequests now return DTOs instead of entities

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/rest/PraktikumsstellenController.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/rest/PraktikumsstellenController.java
@@ -1,11 +1,14 @@
 package de.muenchen.oss.praktikumsplaner.rest;
 
+import de.muenchen.oss.praktikumsplaner.domain.AusbildungsPraktikumsstelle;
 import de.muenchen.oss.praktikumsplaner.domain.BasePraktikumsstelle;
+import de.muenchen.oss.praktikumsplaner.domain.StudiumsPraktikumsstelle;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.AusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.PraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.StudiumsPraktikumsstelleDTO;
+import de.muenchen.oss.praktikumsplaner.domain.mappers.PraktikumsstellenMapper;
 import de.muenchen.oss.praktikumsplaner.service.PraktikumsstellenService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -19,12 +22,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/praktikumsstellen")
 public class PraktikumsstellenController {
 
     private final PraktikumsstellenService praktikumsstellenService;
+    private final PraktikumsstellenMapper praktikumsstellenMapper;
 
     @PreAuthorize("!hasRole('ROLE_' + T(de.muenchen.oss.praktikumsplaner.security.AuthoritiesEnum).NWK.name())")
     @PostMapping("/studium")
@@ -44,7 +51,20 @@ public class PraktikumsstellenController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Iterable<BasePraktikumsstelle> getAllPraktikumsstellen(@RequestParam(name = "status", required = false) String status) {
-        return praktikumsstellenService.getAllPraktiumsstellen();
+    public Iterable<PraktikumsstelleDTO> getAllPraktikumsstellen(@RequestParam(name = "status", required = false) String status) {
+        List<PraktikumsstelleDTO> praktikumsstelleDTOS = new ArrayList<>();
+
+        Iterable<BasePraktikumsstelle> praktikumsstellen = praktikumsstellenService.getAllPraktiumsstellen();
+
+        for (BasePraktikumsstelle praktikumsstelle : praktikumsstellen) {
+            if (praktikumsstelle instanceof AusbildungsPraktikumsstelle) {
+                AusbildungsPraktikumsstelle ausbildung = (AusbildungsPraktikumsstelle) praktikumsstelle;
+                praktikumsstelleDTOS.add(praktikumsstellenMapper.toDTO(ausbildung));
+            } else if (praktikumsstelle instanceof StudiumsPraktikumsstelle) {
+                StudiumsPraktikumsstelle studium = (StudiumsPraktikumsstelle) praktikumsstelle;
+                praktikumsstelleDTOS.add(praktikumsstellenMapper.toDTO(studium));
+            }
+        }
+        return praktikumsstelleDTOS;
     }
 }
