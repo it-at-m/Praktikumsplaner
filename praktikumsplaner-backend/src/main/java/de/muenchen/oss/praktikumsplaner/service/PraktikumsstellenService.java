2cf2b68737f513cb7735412eddf5494382a3282c
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-21 14:57:31 +0100

    :art: added method to get currentMeldezeitraum and set if exists

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
@@ -1,5 +1,6 @@
 package de.muenchen.oss.praktikumsplaner.service;
 
+import de.muenchen.oss.praktikumsplaner.domain.Meldezeitraum;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.AusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleDTO;
@@ -8,11 +9,10 @@ import de.muenchen.oss.praktikumsplaner.domain.mappers.PraktikumsstellenMapper;
 import de.muenchen.oss.praktikumsplaner.repository.AusbildungsPraktikumsstellenRepository;
 import de.muenchen.oss.praktikumsplaner.repository.MeldezeitraumRepository;
 import de.muenchen.oss.praktikumsplaner.repository.StudiumsPraktikumsstellenRepository;
+import jakarta.validation.ValidationException;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.DateTimeException;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @Service
@@ -24,16 +24,27 @@ public class PraktikumsstellenService {
 
     private final MeldezeitraumRepository meldezeitraumRepository;
 
-    public StudiumsPraktikumsstelleDTO saveStudiumsPraktikumsstelle(final CreateStudiumsPraktikumsstelleDTO createStudiumsPraktikumsstelleDTO) {
-        meldezeitraumRepository.findAll().forEach(meldezeitraum -> {
-            if(!(LocalDate.now().isAfter(meldezeitraum.getStartZeitpunkt()) &&
-                LocalDate.now().isBefore(meldezeitraum.getEndZeitpunkt()))) throw new DateTimeException("");
-        });
+    public StudiumsPraktikumsstelleDTO saveStudiumsPraktikumsstelle(CreateStudiumsPraktikumsstelleDTO createStudiumsPraktikumsstelleDTO) {
+        createStudiumsPraktikumsstelleDTO = createStudiumsPraktikumsstelleDTO.withId(getCurrentMeldezeitraum().getId());
         return praktikumsstellenMapper.toDTO(studiumsPraktikumsstellenRepository.save(praktikumsstellenMapper.toEntity(createStudiumsPraktikumsstelleDTO)));
     }
 
-    public AusbildungsPraktikumsstelleDTO saveAusbildungsPraktikumsstelle(final CreateAusbildungsPraktikumsstelleDTO createAusbildungsPraktikumsstelleDTO) {
+    public AusbildungsPraktikumsstelleDTO saveAusbildungsPraktikumsstelle(CreateAusbildungsPraktikumsstelleDTO createAusbildungsPraktikumsstelleDTO) {
+        createAusbildungsPraktikumsstelleDTO = createAusbildungsPraktikumsstelleDTO.withId(getCurrentMeldezeitraum().getId());
         return praktikumsstellenMapper
                 .toDTO(ausbildungsPraktikumsstellenRepository.save(praktikumsstellenMapper.toEntity(createAusbildungsPraktikumsstelleDTO)));
+    }
+
+    public Meldezeitraum getCurrentMeldezeitraum() {
+        Meldezeitraum currentMeldezeitraum = null;
+        for (Meldezeitraum meldezeitraum : meldezeitraumRepository.findAll()) {
+            // plus and minus make reports the same day as start or end possible
+            if (LocalDate.now().isAfter(meldezeitraum.getStartZeitpunkt().minusDays(1)) &&
+                    LocalDate.now().isBefore(meldezeitraum.getEndZeitpunkt().plusDays(1))) {
+                currentMeldezeitraum = meldezeitraum;
+            }
+        }
+        if (currentMeldezeitraum == null) throw new ValidationException("Kein aktiver Meldezeitraum");
+        return currentMeldezeitraum;
     }
 }
