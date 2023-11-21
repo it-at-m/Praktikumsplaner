86a1534c8e16b3998387231377d99b5040de7311
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-21 14:28:44 +0100

    💄🚧 added functional wip design

--- praktikumsplaner-frontend/frontend/src/components/PraktikumsstellenList.vue
+++ praktikumsplaner-frontend/frontend/src/components/PraktikumsstellenList.vue
@@ -1,26 +1,78 @@
 <template>
     <div>
         <v-container>
-            <div
-                v-for="(praktikumsstellenliste, abteilung) in praktikumsstellen"
-                :key="abteilung"
-            >
-                <v-subheader>{{ abteilung }}</v-subheader>
-                <v-list>
-                    <v-list-item-group>
-                        <v-list-item
-                            v-for="praktikumsstelle in asPraktikumsstelleList(
-                                praktikumsstellenliste
-                            )"
-                            :key="praktikumsstelle.id"
-                        >
-                            <v-list-item-content>{{
-                                praktikumsstelle.email
-                            }}</v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
-            </div>
+            <v-expansion-panels multiple>
+                <v-expansion-panel
+                    v-for="(
+                        praktikumsstellenliste, abteilung
+                    ) in praktikumsstellen"
+                    :key="abteilung"
+                >
+                    <v-expansion-panel-header>{{
+                        abteilung
+                    }}</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <v-list>
+                            <v-list-item-group>
+                                <v-list-item
+                                    v-for="praktikumsstelle in asPraktikumsstelleList(
+                                        praktikumsstellenliste
+                                    )"
+                                    :key="praktikumsstelle.id"
+                                >
+                                    <v-list-item-content>
+                                        <v-list-item-title>
+                                            Stelle bei
+                                            {{
+                                                praktikumsstelle.dienststelle
+                                            }}</v-list-item-title
+                                        >
+                                        <v-list-item-subtitle
+                                            v-if="praktikumsstelle.studienart"
+                                        >
+                                            Studiengang:
+                                            {{
+                                                praktikumsstelle.studienart
+                                            }}</v-list-item-subtitle
+                                        >
+                                        <v-list-item-subtitle v-else>
+                                            Ausbildungsrichtung:
+                                            {{
+                                                praktikumsstelle.ausbildungsrichtung
+                                            }}</v-list-item-subtitle
+                                        >
+                                        <v-list-item-subtitle
+                                            v-if="
+                                                praktikumsstelle.studiensemester
+                                            "
+                                            >Studiensemester:
+                                            {{
+                                                praktikumsstelle.studiensemester
+                                            }}</v-list-item-subtitle
+                                        >
+                                        <v-list-item-subtitle v-else>
+                                            Ausbildungsjahr:
+                                            {{
+                                                praktikumsstelle.ausbildungsjahr
+                                            }}</v-list-item-subtitle
+                                        >
+                                        <v-list-item-subtitle
+                                            v-if="
+                                                praktikumsstelle.namentlicheAnforderung
+                                            "
+                                        >
+                                            Namentliche Anforderung:
+                                            {{
+                                                praktikumsstelle.namentlicheAnforderung
+                                            }}
+                                        </v-list-item-subtitle>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list-item-group>
+                        </v-list>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-container>
     </div>
 </template>
