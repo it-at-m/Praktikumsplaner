04e4e7cc33484807d3ef9429b0d6e61468e0df76
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-22 14:48:46 +0100

    :art: changed search to database query

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/MeldezeitraumRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/MeldezeitraumRepository.java
@@ -1,8 +1,13 @@
 package de.muenchen.oss.praktikumsplaner.repository;
 
 import de.muenchen.oss.praktikumsplaner.domain.Meldezeitraum;
+import java.time.LocalDate;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MeldezeitraumRepository extends CrudRepository<Meldezeitraum, UUID> {
+    @Query("SELECT e FROM Meldezeitraum e WHERE :date >= e.startZeitpunkt AND :date <= e.endZeitpunkt")
+    Meldezeitraum findMeldezeitraumByDate(@Param("date") LocalDate date);
 }
