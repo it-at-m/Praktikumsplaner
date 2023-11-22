da89422bad00d4a675fc892a3c68e796c506cee1
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-22 11:20:43 +0100

    :art: moved getCurrentMeldezeitraum to MeldezeitraumService and made to return DTO

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/MeldezeitraumService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/MeldezeitraumService.java
@@ -1,9 +1,12 @@
 package de.muenchen.oss.praktikumsplaner.service;
 
+import de.muenchen.oss.praktikumsplaner.domain.Meldezeitraum;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateMeldezeitraumDTO;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.MeldezeitraumDTO;
 import de.muenchen.oss.praktikumsplaner.domain.mappers.MeldezeitraumMapper;
 import de.muenchen.oss.praktikumsplaner.repository.MeldezeitraumRepository;
+import jakarta.validation.ValidationException;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,18 @@ public class MeldezeitraumService {
 
     public MeldezeitraumDTO createMeldezeitraum(final CreateMeldezeitraumDTO meldezeitraumCreateDto) {
         return meldezeitraumMapper.toDto(meldezeitraumRepository.save(meldezeitraumMapper.toEntity(meldezeitraumCreateDto)));
+    }
+
+    public MeldezeitraumDTO getCurrentMeldezeitraum() {
+        Meldezeitraum currentMeldezeitraum = null;
+        for (Meldezeitraum meldezeitraum : meldezeitraumRepository.findAll()) {
+            // plus and minus make reports the same day as start or end possible
+            if (LocalDate.now().isAfter(meldezeitraum.getStartZeitpunkt().minusDays(1)) &&
+                    LocalDate.now().isBefore(meldezeitraum.getEndZeitpunkt().plusDays(1))) {
+                currentMeldezeitraum = meldezeitraum;
+            }
+        }
+        if (currentMeldezeitraum == null) throw new ValidationException("Kein aktiver Meldezeitraum");
+        return meldezeitraumMapper.toDto(currentMeldezeitraum);
     }
 }
