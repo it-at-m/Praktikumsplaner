7abab1419d28d7f4374e8db5ad6e7078be7ccd0c
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2023-11-27 15:38:13 +0100

    :art: fix some ide warnings

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/BasePraktikumsstelle.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/BasePraktikumsstelle.java
@@ -22,7 +22,7 @@ import org.hibernate.annotations.JdbcTypeCode;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 public abstract class BasePraktikumsstelle extends BaseEntity {
 
     @NotNull
