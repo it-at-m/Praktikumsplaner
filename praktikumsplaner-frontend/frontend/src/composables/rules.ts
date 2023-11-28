b6628268e047ffb1e7ac7b6cf4e135f8cd965391
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-28 09:53:33 +0100

    🦺 made maxLengthRule not mistakenly check for empty string

--- praktikumsplaner-frontend/frontend/src/composables/rules.ts
+++ praktikumsplaner-frontend/frontend/src/composables/rules.ts
@@ -6,7 +6,8 @@ export function useRules() {
 
     function maxLengthRule(length: number, message = "error") {
         return (value: string | null | undefined) =>
-            (value && value.length <= length) || message;
+            ((value || value?.trim() == "") && value.length <= length) ||
+            message;
     }
 
     function notEmptyDateRule(message = "error") {
