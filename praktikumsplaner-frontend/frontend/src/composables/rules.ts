a0c9fd9fcb4f30ba7c68dfab2f27105458c5e88e
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-11-28 15:31:02 +0100

    🦺 changed email rules length

--- praktikumsplaner-frontend/frontend/src/composables/rules.ts
+++ praktikumsplaner-frontend/frontend/src/composables/rules.ts
@@ -26,7 +26,10 @@ export function useRules() {
 
     function emailRule(message = "error") {
         return (value: string | null | undefined) =>
-            (value && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) ||
+            (value &&
+                /^[A-Z0-9._%+-]{1,64}@[A-Z0-9.-]{1,63}\.[A-Z]{1,63}$/i.test(
+                    value
+                )) ||
             message;
     }
 
