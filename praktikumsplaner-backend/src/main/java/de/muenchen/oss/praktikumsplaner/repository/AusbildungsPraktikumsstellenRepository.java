c0afe564d9b91cbfe54da74ea25956d96b95c164
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-11-29 11:58:38 +0100

    🎨 Outsourced repository

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
@@ -3,10 +3,9 @@ package de.muenchen.oss.praktikumsplaner.repository;
 import de.muenchen.oss.praktikumsplaner.domain.AusbildungsPraktikumsstelle;
 import java.util.List;
 import java.util.UUID;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AusbildungsPraktikumsstellenRepository extends CrudRepository<AusbildungsPraktikumsstelle, UUID> {
+public interface AusbildungsPraktikumsstellenRepository extends PraktikumsstellenRepository<AusbildungsPraktikumsstelle> {
     List<AusbildungsPraktikumsstelle> findAllByMeldezeitraumID(UUID id);
 }
