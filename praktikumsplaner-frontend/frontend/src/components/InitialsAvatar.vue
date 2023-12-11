2f6240b502b45e28ab15547a6fa5822b24e9b3f5
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-11 10:31:35 +0100

    💄 Colors added to make the application more vivid

--- praktikumsplaner-frontend/frontend/src/components/InitialsAvatar.vue
+++ praktikumsplaner-frontend/frontend/src/components/InitialsAvatar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-avatar
-        color="accent"
+        :color="pickRandomColor()"
         size="40"
     >
         <span> {{ initials }} </span>
@@ -17,6 +17,22 @@ const initials = computed(() => {
     const names = props.nwkName.split(" ");
     return names.map((name) => name.charAt(0).toUpperCase()).join("");
 });
+const colors = [
+    "#FFC0CB", //Rosa
+    "#FFFF00", //Gelb
+    "#7FFFD4", //Aquamarin
+    "#FF4500", //Orange-Rot
+    "#ADD8E6", //Hellblau
+    "#FF69B4", //Kräftiges Rosa
+    "#9370DB", //Lila
+    "#3CB371", //Dunkelgrün
+    "#FFA500", //Orange
+    "#7FFF00", //Helles grün
+];
+
+function pickRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
 </script>
 <style scoped>
 .v-avatar {
