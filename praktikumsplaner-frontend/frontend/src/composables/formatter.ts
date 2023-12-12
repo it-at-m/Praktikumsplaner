eb8bbcb912e7af3648fd049621602268da03a0fa
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-12-12 15:04:41 +0100

    :white_check_mark: added formatter tests

--- praktikumsplaner-frontend/frontend/src/composables/formatter.ts
+++ praktikumsplaner-frontend/frontend/src/composables/formatter.ts
@@ -1,17 +1,17 @@
 import moment from "moment";
 
 export function useFormatter() {
-    function startingCharUpperCase(text: string): string {
+    function startingCharUpperCase(text: string | undefined | null): string {
         return text
             ? text.toLowerCase().replace(/^\w/, (c) => c.toUpperCase())
             : "";
     }
 
-    function formatDate(date: Date): string {
+    function formatDate(date: Date | undefined | null): string {
         return date ? moment(date).format("L") : "";
     }
 
-    function formatStringDate(date: string | undefined): string {
+    function formatStringDate(date: string | undefined | null): string {
         if (!date) return "";
         const parts = date.split("-");
         return parts[2] + "." + parts[1] + "." + parts[0];
