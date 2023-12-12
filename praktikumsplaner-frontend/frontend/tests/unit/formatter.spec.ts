b71f58ee30a2a2abd12b92dc88d9cc1e97ce39dc
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-12-12 15:04:41 +0100

    :white_check_mark: added formatter tests

---
+++ praktikumsplaner-frontend/frontend/tests/unit/formatter.spec.ts
@@ -0,0 +1,51 @@
+import { describe } from "vitest";
+import { useFormatter } from "@/composables/formatter";
+const formatter = useFormatter();
+describe("formatter startingCharUpperCase test", () => {
+    it("tests startingCharUpperCase to be true", () => {
+        const text = "lorem ipsum";
+        expect(formatter.startingCharUpperCase(text)).toBe("Lorem ipsum");
+    });
+    it("tests startingCharUpperCase to be true when empty", () => {
+        const text = "";
+        expect(formatter.startingCharUpperCase(text)).toBe("");
+    });
+    it("tests startingCharUpperCase to be true when null", () => {
+        expect(formatter.startingCharUpperCase(null)).toBe("");
+    });
+    it("tests startingCharUpperCase to be true when undefined", () => {
+        expect(formatter.startingCharUpperCase(undefined)).toBe("");
+    });
+});
+describe("formatter formatDate test", () => {
+    it("tests formatDate to be true", () => {
+        const date = new Date("2021-05-01");
+        expect(formatter.formatDate(date)).toBe("01.05.2021");
+    });
+    it("tests formatDate to be true when empty", () => {
+        const date = new Date("");
+        expect(formatter.formatDate(date)).toBe("");
+    });
+    it("tests formatDate to be true when null", () => {
+        expect(formatter.formatDate(null)).toBe("");
+    });
+    it("tests formatDate to be true when undefined", () => {
+        expect(formatter.formatDate(undefined)).toBe("");
+    });
+});
+describe("formatter formatStringDate test", () => {
+    it("tests formatStringDate to be true", () => {
+        const date = "2021-05-01";
+        expect(formatter.formatStringDate(date)).toBe("01.05.2021");
+    });
+    it("tests formatStringDate to be true when empty", () => {
+        const date = "";
+        expect(formatter.formatStringDate(date)).toBe("");
+    });
+    it("tests formatStringDate to be true when null", () => {
+        expect(formatter.formatStringDate(null)).toBe("");
+    });
+    it("tests formatStringDate to be true when undefined", () => {
+        expect(formatter.formatStringDate(undefined)).toBe("");
+    });
+});
