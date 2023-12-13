807a7f5a55e9cc1a609dfae257084a40f8ed55b4
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-13 16:37:37 +0100

    🧪 added not working test :(

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/ZeitraumDto.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/ZeitraumDto.java
@@ -1,8 +1,11 @@
 package de.muenchen.oss.praktikumsplaner.domain.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
 import java.time.LocalDate;
 
+@Builder
 public record ZeitraumDto(@NotNull LocalDate startZeitpunkt,
                           @NotNull LocalDate endZeitpunkt) {
 }
