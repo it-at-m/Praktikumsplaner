5ebcb408aec4715ada622e170c6a049345ed02c8
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-14 09:02:26 +0100

    🥅✨ Undelivered mails are now visible after trying to send them

--- praktikumsplaner-frontend/frontend/src/api/MailService.ts
+++ praktikumsplaner-frontend/frontend/src/api/MailService.ts
@@ -3,27 +3,40 @@ import { API_BASE, MAIL_BASE } from "@/Constants";
 import FetchUtils from "@/api/FetchUtils";
 import { useSnackbarStore } from "@/stores/snackbar";
 import { Levels } from "@/api/error";
+import Praktikumsstelle from "@/types/Praktikumsstelle";
 
 export default {
-    sendSuccessfulAssignedMails(assignmentPeriods: { [k: string]: Zeitraum }) {
+    sendSuccessfulAssignedMails(assignmentPeriods: {
+        [k: string]: Zeitraum;
+    }): Promise<Praktikumsstelle[]> {
         return fetch(
             `${API_BASE}${MAIL_BASE}/send?assignmentStatus=successful`,
             FetchUtils.getPOSTConfig(assignmentPeriods)
         )
             .then((response) => {
-                useSnackbarStore().showMessage({
-                    message: "☑ Versenden der Mails war erfolgreich.",
-                    level: Levels.SUCCESS,
-                });
-                FetchUtils.defaultResponseHandler(response);
-                return response.json();
+                if (response.ok) {
+                    // Prüfen, ob der Statuscode OK (200) ist
+                    useSnackbarStore().showMessage({
+                        message: "☑ Alle Mails wurden erfolgreich versendet.",
+                        level: Levels.SUCCESS,
+                    });
+                    return [];
+                } else {
+                    return response.json().then((praktikumsplaetze) => {
+                        useSnackbarStore().showMessage({
+                            message: "Fehler beim Versenden einiger Mails.",
+                            level: Levels.ERROR,
+                        });
+                        return praktikumsplaetze; // Liste der betroffenen Praktikumsplätze zurückgeben
+                    });
+                }
             })
             .catch((err) => {
                 useSnackbarStore().showMessage({
                     message: err.message,
                     level: Levels.ERROR,
                 });
-                FetchUtils.defaultResponseHandler(err);
+                return Promise.reject(err); // Fehler zurückgeben
             });
     },
 };
---
+++ praktikumsplaner-frontend/frontend/src/components/Assignment/undeliveredMailsDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-dialog
+        :value="props.showUndeliveredMailsDialog"
+        persistent
+        max-width="850px"
+    >
+        <v-card>
+            <v-card-title>
+                Die folgenden Praktikumsstellen konnten nicht benachrichtigt
+                werden:
+            </v-card-title>
+            <v-card-text>
+                <div
+                    v-for="stelle in props.faultyStellen"
+                    :key="stelle.id"
+                >
+                    <h4>Stelle bei {{ stelle.dienststelle }}</h4>
+                    <p>
+                        Ausbilder*in: {{ stelle.email }} (Zugewiesene NWK:
+                        {{ stelle.assignedNwk }})
+                    </p>
+                </div>
+                <v-divider class="grey" />
+            </v-card-text>
+            <v-card-actions>
+                <v-row class="mb-2">
+                    <v-col class="col-auto mr-auto">
+                        <v-btn
+                            color="primary"
+                            @click="closeDialog"
+                        >
+                            Schließen
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+<script setup lang="ts">
+import Praktikumsstelle from "@/types/Praktikumsstelle";
+
+const props = defineProps<{
+    faultyStellen: Praktikumsstelle[];
+    showUndeliveredMailsDialog: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: "update:showUndeliveredMailsDialog", b: boolean): void;
+}>();
+
+function closeDialog(): void {
+    emit("update:showUndeliveredMailsDialog", false);
+}
+</script>
