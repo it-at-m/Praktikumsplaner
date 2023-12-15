95ec107356eb1a17f1318f589f6c4706f9c49754
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2023-12-15 14:22:05 +0100

    🔀 fixed merge conflicts

--- praktikumsplaner-frontend/frontend/src/views/Praktikumsplaetze/PraktikumsplaetzeView.vue
+++ praktikumsplaner-frontend/frontend/src/views/Praktikumsplaetze/PraktikumsplaetzeView.vue
@@ -6,9 +6,25 @@
         ></page-title>
 
         <div v-if="!activeMeldezeitraum">
-            Ihre örtliche Ausbildungsleitung hat die Meldung von Stellen noch
-            nicht freigegeben, daher können aktuell leider noch keine keine
-            Praktikumsplätze gemeldet werden.
+            <v-row class="align-center">
+                <v-col
+                    cols="auto"
+                    class="d-flex align-center"
+                >
+                    <v-icon
+                        color="blue"
+                        large
+                        >mdi-information-outline</v-icon
+                    >
+                </v-col>
+                <v-col class="d-flex align-center">
+                    <p class="mt-5">
+                        Ihre örtliche Ausbildungsleitung hat die Meldung von
+                        Stellen noch nicht freigegeben, daher können aktuell
+                        leider noch keine Praktikumsplätze gemeldet werden.
+                    </p>
+                </v-col>
+            </v-row>
         </div>
         <div
             v-else
