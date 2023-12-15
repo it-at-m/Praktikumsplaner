fe44bb353e54f7dd07c4f2f4e1d8e7ce731e568e
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-15 11:21:51 +0100

    ✅ 100% Exception Coverage

--- praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/exception/GlobalExceptionHandlerTest.java
+++ praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/exception/GlobalExceptionHandlerTest.java
@@ -3,8 +3,12 @@ package de.muenchen.oss.praktikumsplaner.exception;
 import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,23 +18,55 @@ public class GlobalExceptionHandlerTest {
     private final GlobalExceptionHandler handler = new GlobalExceptionHandler();
 
     @Test
-    void testValidationException() {
+    public void testValidationException() {
         ValidationException ex = new ValidationException("Fehlermeldung");
         String response = handler.validationException(ex);
         assertEquals("Fehlermeldung", response);
     }
 
     @Test
-    void testIoException() {
+    public void testExcelImportException() {
+        ExcelImportException.ExcelImportExceptionInfo exceptionInfo = new ExcelImportException.ExcelImportExceptionInfo(1, "ColumnName", "InvalidValue");
+        ExcelImportException excelImportException = new ExcelImportException(List.of(exceptionInfo));
+        String response = handler.ExcelImportException(excelImportException);
+        assertEquals(excelImportException.getExceptionInfos().toString(), response);
+    }
+
+    @Test
+    public void testIoException() {
         IOException ex = new IOException("Fehlermeldung");
         String response = handler.ioException(ex);
         assertEquals("Fehlermeldung", response);
     }
 
     @Test
-    void testIllegalArgumentException() {
+    public void testIllegalArgumentException() {
         IllegalArgumentException ex = new IllegalArgumentException("Fehlermeldung");
         String response = handler.IllegalArgumentException(ex);
         assertEquals("Fehlermeldung", response);
+    }
+
+    @Test
+    public void testHandleNotFoundException() {
+        ResourceNotFoundException ex = new ResourceNotFoundException("Fehlermeldung");
+        ResponseEntity<String> response = handler.handleNotFoundException(ex);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Fehlermeldung", response.getBody());
+    }
+
+    @Test
+    public void testHandleNoSuchElementException() {
+        NoSuchElementException ex = new NoSuchElementException("Fehlermeldung");
+        ResponseEntity<String> response = handler.handleNoSuchElementException(ex);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Fehlermeldung", response.getBody());
+    }
+
+    @Test
+    public void testHandleConflictException() {
+        ResourceConflictException ex = new ResourceConflictException("Fehlermeldung");
+        ResponseEntity<String> response = handler.handleConflictException(ex);
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals("Fehlermeldung", response.getBody());
     }
 }
