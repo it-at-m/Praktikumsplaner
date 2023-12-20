f9577dfe09c374b4665055c63e30b449a882700d
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-20 10:25:07 +0100

    Revert "✏️ fixed Typo"
    This reverts commit 76fa921bc4b7873680b6f7648f39218ee5387abd.

--- praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenServiceTest.java
+++ praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenServiceTest.java
@@ -174,7 +174,7 @@ public class PraktikumsstellenServiceTest {
         when(mapper.toDto(any(StudiumsPraktikumsstelle.class)))
                 .thenAnswer(invocation -> createPraktikumsstelleDto((StudiumsPraktikumsstelle) invocation.getArguments()[0]));
 
-        TreeMap<String, List<PraktikumsstelleDto>> result = service.getAllPraktikumsstellenInMostRecentPassedMeldezeitraum();
+        TreeMap<String, List<PraktikumsstelleDto>> result = service.getAllPraktiumsstellenInMostRecentPassedMeldezeitraum();
 
         assertNotNull(result);
         assertEquals(4, result.size());
