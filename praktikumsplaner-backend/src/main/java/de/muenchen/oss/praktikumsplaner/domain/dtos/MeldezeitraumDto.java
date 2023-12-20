8de73a6ff33565c09dd0a0b34f7f4fa7158f184c
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2023-12-20 10:19:39 +0100

    🚑️ MeldezeitraumDtos now with ZeitraumDto

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDto.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/MeldezeitraumDto.java
@@ -1,20 +1,16 @@
 package de.muenchen.oss.praktikumsplaner.domain.dtos;
 
-import de.muenchen.oss.praktikumsplaner.annotations.StartDateBeforeEndDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
 import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-@StartDateBeforeEndDate(startDate = "startZeitpunkt", endDate = "endZeitpunkt", message = "Startdatum muss vor dem Enddatum liegen.")
 public record MeldezeitraumDto(
         @NotNull UUID id,
         @Size(max = 255, message = "Name für Zeitraum darf nicht länger als {max} Zeichen sein.")
         @NotBlank
         String zeitraumName,
-        @NotNull LocalDate startZeitpunkt,
-        @NotNull LocalDate endZeitpunkt)
+        ZeitraumDto zeitraum)
 {}
--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/ZeitraumDto.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/dtos/ZeitraumDto.java
@@ -1,10 +1,12 @@
 package de.muenchen.oss.praktikumsplaner.domain.dtos;
 
+import de.muenchen.oss.praktikumsplaner.annotations.StartDateBeforeEndDate;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
+@StartDateBeforeEndDate(startDate = "startZeitpunkt", endDate = "endZeitpunkt", message = "Startdatum muss vor dem Enddatum liegen.")
 public record ZeitraumDto(@NotNull LocalDate startZeitpunkt,
                           @NotNull LocalDate endZeitpunkt) {
 }
