da8053a2cbc0b446b1227bca8060a620e24af552
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-01-02 12:18:12 +0100

    🎨 spotless & checkstyle

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/configuration/MailConfiguration.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/configuration/MailConfiguration.java
@@ -1,22 +1,20 @@
 package de.muenchen.oss.praktikumsplaner.configuration;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
-
 @Configuration
 public class MailConfiguration {
 
     @Bean
-    public JavaMailSender getJavaMailSender(  @Value("${spring.mail.password}") String password,
-                                              @Value("${spring.mail.username}") String username,
-                                              @Value("${spring.mail.host}") String host,
-                                              @Value("${spring.mail.port}") int port)
-    {
+    public JavaMailSender getJavaMailSender(@Value("${spring.mail.password}") String password,
+            @Value("${spring.mail.username}") String username,
+            @Value("${spring.mail.host}") String host,
+            @Value("${spring.mail.port}") int port) {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(host);
         mailSender.setPort(port);
