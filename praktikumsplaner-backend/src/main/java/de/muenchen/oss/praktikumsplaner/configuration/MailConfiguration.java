dd00f1f3eb67db450920509469013cbaa4817cc8
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-01-02 12:16:39 +0100

    🔧 added mail configuration

---
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/configuration/MailConfiguration.java
@@ -0,0 +1,35 @@
+package de.muenchen.oss.praktikumsplaner.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender(  @Value("${spring.mail.password}") String password,
+                                              @Value("${spring.mail.username}") String username,
+                                              @Value("${spring.mail.host}") String host,
+                                              @Value("${spring.mail.port}") int port)
+    {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
