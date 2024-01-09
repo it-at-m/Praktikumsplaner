9602b9a3e038506bd64b7ebd6513f05880494ca3
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2024-01-09 17:37:46 +0100

    🔀🐛 Bug-Fix: NWK List in Assing view (#181)
    * :sparkles: warning dialog component init
    
    * :art: refactor assign view to make it easier compatible witzh warning dialog
    
    * :fire: removed unused import and method
    
    * :sparkles: implemented function to generate warnings
    
    * :bug: praktikumsstellen are displayed correctly now
    
    * :bug: assignednwk are displayed correctly now
    
    * :memo: added the warning dialog to documentation
    
    * :sparkles: Added Export feature
    
    * :white_check_mark: improved tests
    
    * :memo: added docs
    
    * :sparkles: added warning dialog before exporting assignments
    
    * Update PraktikumsplaetzeView.vue
    
    * Update WarningDialog.vue
    
    * 🚨 removed double imports and unresolvable stuff
    
    * :bug: nwks are now in assign list when they are assigned to a Praktikumsstelle in an older meldezeitraum
    
    ---------
    
    Co-authored-by: Luis Einig <69314043+LuisEinig@users.noreply.github.com>

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/NwkService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/NwkService.java
@@ -7,6 +7,7 @@ import de.muenchen.oss.praktikumsplaner.domain.mappers.NwkMapper;
 import de.muenchen.oss.praktikumsplaner.repository.NwkRepository;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,7 @@ public class NwkService {
     private final NwkMapper nwkMapper;
     private final NwkRepository nwkRepository;
     private final ExcelImportService excelImportService;
+    private final MeldezeitraumService meldezeitraumService;
 
     private static final Logger logger = LoggerFactory.getLogger(NwkService.class);
 
@@ -37,8 +39,9 @@ public class NwkService {
         return nwkRepository.findNwksByActiveIsTrueOrderByNachname().stream().map(nwkMapper::toDto).toList();
     }
 
-    public List<NwkDto> findAllUnassignedNwks() {
-        return nwkRepository.findAllUnassigned().stream().map(nwkMapper::toDto).toList();
+    public List<NwkDto> findAllUnassignedNwksInCurrentMeldezeitraum() {
+        final UUID meldezeitraumId = meldezeitraumService.getMostRecentPassedMeldezeitraum().id();
+        return nwkRepository.findAllUnassignedInSpecificMeldzeitraum(meldezeitraumId).stream().map(nwkMapper::toDto).toList();
     }
 
     public List<NwkDto> findAllNwks() {
