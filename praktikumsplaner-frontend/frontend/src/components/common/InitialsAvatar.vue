e8b7304767b56a77a3d5d2ebe2e3af27e261bf45
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-01-11 11:36:46 +0100

    ✨ InitialAvatar Color by Studiengang / Ausbildungsrichtung

--- praktikumsplaner-frontend/frontend/src/components/common/InitialsAvatar.vue
+++ praktikumsplaner-frontend/frontend/src/components/common/InitialsAvatar.vue
@@ -1,6 +1,6 @@
 <template>
     <v-avatar
-        :color="pickRandomColor()"
+        :color="props.backgroundColor"
         size="40"
     >
         <span> {{ initials }} </span>
@@ -11,28 +11,13 @@ import { computed, defineProps } from "vue";
 
 const props = defineProps<{
     nwkName: string;
+    backgroundColor: string;
 }>();
 
 const initials = computed(() => {
     const names = props.nwkName.split(" ");
     return names.map((name) => name.charAt(0).toUpperCase()).join("");
 });
-const colors = [
-    "#FFC0CB", //Rosa
-    "#FFFF00", //Gelb
-    "#7FFFD4", //Aquamarin
-    "#FF4500", //Orange-Rot
-    "#ADD8E6", //Hellblau
-    "#FF69B4", //Kräftiges Rosa
-    "#9370DB", //Lila
-    "#3CB371", //Dunkelgrün
-    "#FFA500", //Orange
-    "#7FFF00", //Helles grün
-];
-
-function pickRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
-}
 </script>
 <style scoped>
 .v-avatar {
--- praktikumsplaner-frontend/frontend/src/types/Ausbildungsrichtung.ts
+++ praktikumsplaner-frontend/frontend/src/types/Ausbildungsrichtung.ts
@@ -1,3 +1,14 @@
 export const Ausbildungsrichtung = [
-    { name: "Fachinformatiker Systemintegration (FISI)", value: "FISI" },
+    {
+        name: "Fachinformatiker Systemintegration (FISI)",
+        value: "FISI",
+        color: "light-blue lighten-4",
+    },
 ] as const;
+
+export function findAusbildungsrichtungColorByValue(value: string): string {
+    const ausbildungsrichtung = Ausbildungsrichtung.find(
+        (s) => s.value === value
+    );
+    return ausbildungsrichtung ? ausbildungsrichtung.color : "";
+}
