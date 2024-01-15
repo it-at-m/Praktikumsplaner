78e1b24647684b1c0915f4f88c81eb943e4403e4
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2024-01-15 14:13:03 +0100

    update spring boot 3.0.7 -> 3.1.5

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/configuration/NoSecurityConfiguration.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/configuration/NoSecurityConfiguration.java
@@ -4,12 +4,16 @@
  */
 package de.muenchen.oss.praktikumsplaner.configuration;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @Profile("no-security")
@@ -20,17 +24,14 @@ public class NoSecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // @formatter:off
         http
-                .headers()
-                    .frameOptions()
-                    .disable()
-                .and().authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/**")
-                                .permitAll()
-                                .anyRequest()
-                                .permitAll()
-                )
-                .csrf()
-                .disable();
+                .headers(customizer -> customizer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(requests -> requests.requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
+                        .permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+
         // @formatter:on
         return http.build();
     }
