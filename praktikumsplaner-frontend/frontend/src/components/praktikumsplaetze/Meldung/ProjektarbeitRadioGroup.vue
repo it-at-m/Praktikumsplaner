0df8c4fd9f8c246a07af86470b0c957d7630bcdd
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2024-01-25 09:38:33 +0100

    :art: Projektarbeit into components

---
+++ praktikumsplaner-frontend/frontend/src/components/praktikumsplaetze/Meldung/ProjektarbeitRadioGroup.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-radio-group
+        v-model="projektarbeit"
+        class="radios custom-label"
+        row
+        :rules="booleanRule"
+    >
+        <template #label>
+            <span class="custom-label">Projektarbeit*:</span>
+        </template>
+        <v-radio
+            v-for="item in YesNoBoolean"
+            :key="item.value"
+            :label="item.name"
+            :value="item.value"
+            class="ml-5"
+        ></v-radio>
+    </v-radio-group>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRules } from "@/composables/rules";
+import { YesNoBoolean } from "@/types/YesNoBoolean";
+
+const validationRules = useRules();
+
+const props = defineProps<{
+    value: boolean;
+}>();
+const emits = defineEmits<{
+    (e: "input", projektarbeit: boolean): void;
+}>();
+
+const booleanRule = [
+    validationRules.notEmptyBooleanRule("Darf nicht leer sein."),
+];
+
+const projektarbeit = computed({
+    // getter
+    get() {
+        return props.value;
+    },
+    // setter
+    set(newValue) {
+        emits("input", newValue);
+    },
+});
+</script>
