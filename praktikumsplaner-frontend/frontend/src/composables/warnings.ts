fb1cd999468b09c66875906f363cc717d543989c
Tuckerle <79114427+Tuckerle@users.noreply.github.com>
2024-01-25 16:59:43 +0100

    :bug: warnings before export or mail sending returned correctly

--- praktikumsplaner-frontend/frontend/src/composables/warnings.ts
+++ praktikumsplaner-frontend/frontend/src/composables/warnings.ts
@@ -184,7 +184,7 @@ export function useWarnings() {
                 warnings.push(warning);
             }
         }
-        return [];
+        return warnings;
     }
 
     function calculateSemester(nwk: Nwk) {
