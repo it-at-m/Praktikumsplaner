d4214d1ee8e1db1db6a71f9a9d48364586f5105e
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-02-14 16:06:31 +0100

    🐛 back to png + added png binary to git attribute

--- praktikumsplaner-frontend/frontend/src/App.vue
+++ praktikumsplaner-frontend/frontend/src/App.vue
@@ -18,7 +18,7 @@
                             max-height="50"
                             max-width="50"
                             contain
-                            src="src/assets/logo.jpg"
+                            src="src/assets/logo.png"
                         ></v-img>
                     </router-link>
                     <router-link to="/">
