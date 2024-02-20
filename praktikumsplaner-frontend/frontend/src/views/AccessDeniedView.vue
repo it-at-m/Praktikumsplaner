c85cc0057141440adf2687c169e0c52206f5e22b
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-02-20 13:32:29 +0100

    💄 fixed layout of accessdeniedview

--- praktikumsplaner-frontend/frontend/src/views/AccessDeniedView.vue
+++ praktikumsplaner-frontend/frontend/src/views/AccessDeniedView.vue
@@ -4,26 +4,25 @@
             back-button-url="/"
             page-header-text="Zurück zur Startseite"
         ></page-title>
-        <div class="m-2 p-2">
-            <v-icon
-                size="large"
-                color="red"
-            >mdi-alert-octagon</v-icon
-            >
-            <h1>Zugriff verweigert</h1>
-            <p class="m-2 p-2">
-                Du hast nicht die erforderlichen Rechte, um auf diese Seite
-                zuzugreifen.
-            </p>
-        </div>
+        <v-row align="center" class="ml-auto">
+            <v-col cols="auto">
+                <v-icon
+                    size="large"
+                    color="red"
+                >mdi-alert-octagon</v-icon
+                >
+            </v-col>
+            <v-col>
+                <h1>Zugriff verweigert</h1>
+                <p class="m-2 p-2">
+                    Du hast nicht die erforderlichen Rechte, um auf diese Seite
+                    zuzugreifen.
+                </p>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script setup lang="ts">
-import router from "@/router";
 import PageTitle from "@/components/common/PageTitle.vue";
-
-function goBack() {
-    router.push({ name: "home" });
-}
 </script>
