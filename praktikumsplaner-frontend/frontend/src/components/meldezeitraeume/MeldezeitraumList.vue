6c2f9e94526bd08e07d5a338019e262864288a72
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2024-02-22 16:30:46 +0100

    :art: refactor with merge

--- praktikumsplaner-frontend/frontend/src/components/meldezeitraeume/MeldezeitraumList.vue
+++ praktikumsplaner-frontend/frontend/src/components/meldezeitraeume/MeldezeitraumList.vue
@@ -1,73 +1,73 @@
 <template>
     <v-container>
         <v-col>
-        <slot name="header"></slot>
-        <v-list v-if="props.value.length > 0">
-            <v-card
-                v-for="meldezeitraum in props.value"
-                :key="meldezeitraum.id"
-                rounded
-                border
-            >
-                <v-divider></v-divider>
-                <v-list-item lines="two">
-                    <v-list-item-title class="justify-center">
-                        {{ meldezeitraum.zeitraumName }}
-                        <slot name="card-title-icon"></slot>
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                        <v-icon>mdi-calendar-start</v-icon>
-                        {{
-                            useFormatter().formatDateFromString(
-                                meldezeitraum.zeitraum.startZeitpunkt
-                            )
-                        }}
+            <slot name="header"></slot>
+            <v-list v-if="props.value.length > 0">
+                <v-card
+                    v-for="meldezeitraum in props.value"
+                    :key="meldezeitraum.id"
+                    rounded
+                    border
+                >
+                    <v-divider></v-divider>
+                    <v-list-item lines="two">
+                        <v-list-item-title class="justify-center">
+                            {{ meldezeitraum.zeitraumName }}
+                            <slot name="card-title-icon"></slot>
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                            <v-icon>mdi-calendar-start</v-icon>
+                            {{
+                                useFormatter().formatDateFromString(
+                                    meldezeitraum.zeitraum.startZeitpunkt
+                                )
+                            }}
 
-                        <v-icon>mdi-calendar-end</v-icon>
-                        {{
-                            useFormatter().formatDateFromString(
-                                meldezeitraum.zeitraum.endZeitpunkt
-                            )
-                        }}
-                    </v-list-item-subtitle>
-                </v-list-item>
-                <v-list-item-action>
-                    <v-btn
-                        icon
-                        @click="warnBeforeDelete(meldezeitraum)"
-                    >
-                        <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                </v-list-item-action>
-                <v-divider></v-divider>
-            </v-card>
-        </v-list>
-        <slot
-            v-else
-            name="notfoundmessage"
-        ></slot>
-    </v-col>
-    <yes-no-dialog-without-activator
-        :dialogtitle="deleteWarningDialogTitle"
-        :dialogtext="deleteWarningDialogText"
-        :value="showDeleteWarningDialog"
-        @no="resetDeleteWarningDialog"
-        @yes="deleteMeldezeitraum"
-    ></yes-no-dialog-without-activator>
-    <progress-circular-overlay
-        :loading="loading"
-    ></progress-circular-overlay>
+                            <v-icon>mdi-calendar-end</v-icon>
+                            {{
+                                useFormatter().formatDateFromString(
+                                    meldezeitraum.zeitraum.endZeitpunkt
+                                )
+                            }}
+                        </v-list-item-subtitle>
+                    </v-list-item>
+                    <v-list-item-action>
+                        <v-btn
+                            variant="text"
+                            icon="mdi-delete"
+                            @click="warnBeforeDelete(meldezeitraum)"
+                        >
+                        </v-btn>
+                    </v-list-item-action>
+                    <v-divider></v-divider>
+                </v-card>
+            </v-list>
+            <slot
+                v-else
+                name="notfoundmessage"
+            ></slot>
+        </v-col>
+        <yes-no-dialog-without-activator
+            :dialogtitle="deleteWarningDialogTitle"
+            :dialogtext="deleteWarningDialogText"
+            :value="showDeleteWarningDialog"
+            @no="resetDeleteWarningDialog"
+            @yes="deleteMeldezeitraum"
+        ></yes-no-dialog-without-activator>
+        <progress-circular-overlay
+            :loading="loading"
+        ></progress-circular-overlay>
     </v-container>
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from "vue";
-import Meldezeitraum from "@/types/Meldezeitraum";
+import { ref } from "vue";
+
+import MeldezeitraumService from "@/api/MeldezeitraumService";
+import ProgressCircularOverlay from "@/components/common/ProgressCircularOverlay.vue";
+import YesNoDialogWithoutActivator from "@/components/common/YesNoDialogWithoutActivator.vue";
 import { useFormatter } from "@/composables/formatter";
 import Meldezeitraum from "@/types/Meldezeitraum";
-import MeldezeitraumService from "@/api/MeldezeitraumService";
-import YesNoDialogWithoutActivator from "@/components/common/YesNoDialogWithoutActivator.vue";
-import ProgressCircularOverlay from "@/components/common/ProgressCircularOverlay.vue";
 
 const showDeleteWarningDialog = ref(false);
 const loading = ref(false);
