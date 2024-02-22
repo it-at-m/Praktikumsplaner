f7345ddb068c5eac9b1fe61a195af4ca4e806477
Luis Einig <69314043+LuisEinig@users.noreply.github.com>
2024-02-22 15:47:39 +0100

    :art: fixed cut off labels and changed to fit create style

--- praktikumsplaner-frontend/frontend/src/components/nachwuchskraefte/NwkUpdateDialog.vue
+++ praktikumsplaner-frontend/frontend/src/components/nachwuchskraefte/NwkUpdateDialog.vue
@@ -19,58 +19,32 @@
                     >
                     <v-list>
                         <v-list-item>
-                            <v-text-field
-                                v-model="nwkToUpdate.vorname"
-                                label="Vorname"
-                                :rules="nameRule"
-                                variant="outlined"
-                            ></v-text-field>
+                            <v-container>
+                                <name-input v-model="nwkToUpdate"></name-input>
+                            </v-container>
                         </v-list-item>
                         <v-list-item>
-                            <v-text-field
-                                v-model="nwkToUpdate.nachname"
-                                label="Nachname"
-                                :rules="nameRule"
-                                variant="outlined"
-                            ></v-text-field>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="6">
+                                        <jahrgang-input
+                                            v-model="nwkToUpdate"
+                                        ></jahrgang-input>
+                                    </v-col>
+                                    <v-col cols="6">
+                                        <vorlesungstage-selector
+                                            v-model="nwkToUpdate"
+                                        ></vorlesungstage-selector>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
                         </v-list-item>
                         <v-list-item>
-                            <v-text-field
-                                v-model="nwkToUpdate.jahrgang"
-                                label="Jahrgang"
-                                :rules="jahrgangRule"
-                                variant="outlined"
-                            ></v-text-field>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-select
-                                v-model="nwkToUpdate.studiengang"
-                                label="Studienrichtung"
-                                :items="Studiengang"
-                                item-value="value"
-                                item-title="name"
-                                variant="outlined"
-                                clearable
-                                :rules="isStudiumOrAusbildungRule"
-                                @click:clear="
-                                    nwkToUpdate.studiengang = undefined
-                                "
-                            ></v-select>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-select
-                                v-model="nwkToUpdate.ausbildungsrichtung"
-                                label="Ausbildungsrichtung"
-                                :items="Ausbildungsrichtung"
-                                item-value="value"
-                                item-title="name"
-                                variant="outlined"
-                                clearable
-                                :rules="isStudiumOrAusbildungRule"
-                                @click:clear="
-                                    nwkToUpdate.ausbildungsrichtung = undefined
-                                "
-                            ></v-select>
+                            <v-container>
+                                <studienrichtung-or-ausbildungsrichtung-select
+                                    v-model="nwkToUpdate"
+                                ></studienrichtung-or-ausbildungsrichtung-select>
+                            </v-container>
                         </v-list-item>
                     </v-list>
                     <v-card-actions>
@@ -100,53 +74,25 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 import NwkService from "@/api/NwkService";
+import JahrgangInput from "@/components/common/JahrgangInput.vue";
+import NameInput from "@/components/common/NameInput.vue";
 import ProgressCircularOverlay from "@/components/common/ProgressCircularOverlay.vue";
-import { useRules } from "@/composables/rules";
-import { Ausbildungsrichtung } from "@/types/Ausbildungsrichtung";
+import StudienrichtungOrAusbildungsrichtungSelect from "@/components/common/StudienrichtungOrAusbildungsrichtungSelect.vue";
+import VorlesungstageSelector from "@/components/nachwuchskraefte/VorlesungstageSelect.vue";
 import Nwk from "@/types/Nwk";
-import { Studiengang } from "@/types/Studiengang";
 
 const visible = ref<boolean>(false);
 const loading = ref<boolean>(false);
 const form = ref<HTMLFormElement>();
-const validationRules = useRules();
-const nameRule = [
-    validationRules.minLengthRule(
-        2,
-        "Der Name muss mindestens 2 Zeichen lang sein."
-    ),
-    validationRules.maxLengthRule(
-        255,
-        "Der Name darf maximal 255 Zeichen lang sein."
-    ),
-    validationRules.notEmptyRule("Der Name darf nicht leer sein."),
-];
-const jahrgangRule = [
-    validationRules.notEmptyRule("Der Jahrgang darf nicht leer sein."),
-    validationRules.regexRule(
-        /^([0-9]{2})\/([0-9]{2})$/,
-        "Der Jahrgang muss im Format XX/XX angegeben werden."
-    ),
-];
 
 const props = defineProps<{
     nwk: Nwk;
 }>();
 
 const nwkToUpdate = ref<Nwk>(props.nwk);
-
-const isStudiumOrAusbildungRule = computed(() => {
-    return [
-        (nwkToUpdate.value.studiengang != undefined &&
-            nwkToUpdate.value.ausbildungsrichtung == undefined) ||
-            (nwkToUpdate.value.studiengang == undefined &&
-                nwkToUpdate.value.ausbildungsrichtung != undefined) ||
-            "Es muss eine Studienrichtung oder eine Ausbildungsrichtung angegeben werden.",
-    ];
-});
 
 const emits = defineEmits<{
     (e: "updated"): void;
