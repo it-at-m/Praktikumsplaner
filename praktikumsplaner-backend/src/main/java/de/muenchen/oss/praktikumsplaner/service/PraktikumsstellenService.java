bb472c21d2670d9d087017f3561c161faaebbc16
Michael Huber <59747867+IamPekka058@users.noreply.github.com>
2024-02-26 16:02:11 +0100

    🔥 Removed unused import

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenService.java
@@ -18,7 +18,6 @@ import de.muenchen.oss.praktikumsplaner.repository.StudiumsPraktikumsstellenRepo
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.TreeMap;
 import java.util.UUID;
 import java.util.regex.Matcher;
