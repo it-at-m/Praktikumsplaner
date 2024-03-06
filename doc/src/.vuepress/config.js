2f00b72b59479db1c4517afd1a0b02e809a3c975
Daniel Nischwitz <95384880+danielnwz@users.noreply.github.com>
2024-03-06 10:18:42 +0100

    📝 added favicon to docu

--- doc/src/.vuepress/config.js
+++ doc/src/.vuepress/config.js
@@ -33,6 +33,7 @@ module.exports = {
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    ['link', { rel: 'icon', href: 'logo.png' }]
   ],
   locales: {
     "/": {
