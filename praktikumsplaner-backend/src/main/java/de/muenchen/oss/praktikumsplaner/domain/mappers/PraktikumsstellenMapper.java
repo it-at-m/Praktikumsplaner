90c93d21f0e41dd91404cb44951b45c56cb47a35
Michael Huber <59747867+IamPekka058@users.noreply.github.com>
2024-03-08 06:47:50 +0100

    🏗️ Implemented new mappings in mapper

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/mappers/PraktikumsstellenMapper.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/domain/mappers/PraktikumsstellenMapper.java
@@ -4,11 +4,14 @@ import de.muenchen.oss.praktikumsplaner.domain.AusbildungsPraktikumsstelle;
 import de.muenchen.oss.praktikumsplaner.domain.StudiumsPraktikumsstelle;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.AusbildungsPraktikumsstelleDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleDto;
+import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleWithMeldezeitraumAndAssignedNWKDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateAusbildungsPraktikumsstelleWithMeldezeitraumDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleDto;
+import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleWithMeldezeitraumAndAssignedNwkDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.CreateStudiumsPraktikumsstelleWithMeldezeitraumDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.MeldezeitraumDto;
 import de.muenchen.oss.praktikumsplaner.domain.dtos.StudiumsPraktikumsstelleDto;
+import java.util.UUID;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -38,4 +41,10 @@ public interface PraktikumsstellenMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "assignedNwk", ignore = true)
     AusbildungsPraktikumsstelle toEntity(final CreateAusbildungsPraktikumsstelleWithMeldezeitraumDto createAusbildungsPraktikumsstelleWithMeldezeitraumDto);
+
+    @Mapping(target = "id", source = "id")
+    AusbildungsPraktikumsstelle toEntity(final UUID id, final CreateAusbildungsPraktikumsstelleWithMeldezeitraumAndAssignedNWKDto ausbildungsPraktikumsstelleDto);
+
+    @Mapping(target="id", source = "id")
+    StudiumsPraktikumsstelle toEntity(final UUID id, final CreateStudiumsPraktikumsstelleWithMeldezeitraumAndAssignedNwkDto studiumsPraktikumsstelleDto);
 }
