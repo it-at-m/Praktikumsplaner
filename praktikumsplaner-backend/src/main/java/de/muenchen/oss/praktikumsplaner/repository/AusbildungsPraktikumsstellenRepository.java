688f4b7c75549e5d5ad73ec73fa223f48e51b96b
Michael Huber <59747867+IamPekka058@users.noreply.github.com>
2024-03-11 09:39:09 +0100

    🗃️ New database update function for Ausbildungs... and StudiumsPraktikumsstelle

--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/AusbildungsPraktikumsstellenRepository.java
@@ -3,7 +3,10 @@ package de.muenchen.oss.praktikumsplaner.repository;
 import de.muenchen.oss.praktikumsplaner.domain.AusbildungsPraktikumsstelle;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +14,8 @@ public interface AusbildungsPraktikumsstellenRepository extends CrudRepository<A
     List<AusbildungsPraktikumsstelle> findAllByMeldezeitraumID(final UUID id);
 
     List<AusbildungsPraktikumsstelle> findAllByMeldezeitraumIDAndAssignedNwkIsNotNull(final UUID id);
+
+    @Modifying
+    @Query("update AusbildungsPraktikumsstelle u set u.dienststelle = :dienststelle, u.email = :email, u.taetigkeiten = :taetigkeiten, u.oertlicheAusbilder = :ausbilder where u.id = :id")
+    void updateAusbildungsPraktikumsstelleWithAssignedNwk(@Param(value = "id") UUID id, @Param(value = "dienststelle") String dienststelle, @Param(value = "ausbilder") String ausbilder, @Param(value = "taetigkeiten") String taetigkeit, @Param(value = "email") String email);
 }
--- praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstellenRepository.java
+++ praktikumsplaner-backend/src/main/java/de/muenchen/oss/praktikumsplaner/repository/StudiumsPraktikumsstellenRepository.java
@@ -3,7 +3,10 @@ package de.muenchen.oss.praktikumsplaner.repository;
 import de.muenchen.oss.praktikumsplaner.domain.StudiumsPraktikumsstelle;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +14,9 @@ public interface StudiumsPraktikumsstellenRepository extends CrudRepository<Stud
     List<StudiumsPraktikumsstelle> findAllByMeldezeitraumID(final UUID id);
 
     List<StudiumsPraktikumsstelle> findAllByMeldezeitraumIDAndAssignedNwkIsNotNull(final UUID id);
+
+    @Modifying
+    @Query("update StudiumsPraktikumsstelle u set u.dienststelle = :dienststelle, u.email = :email, u.taetigkeiten = :taetigkeiten, u.oertlicheAusbilder = :ausbilder where u.id = :id")
+    void updateStudiumssPraktikumsstelleWithAssignedNwk(@Param(value = "id") UUID id, @Param(value = "dienststelle") String dienststelle, @Param(value = "ausbilder") String ausbilder, @Param(value = "taetigkeiten") String taetigkeit, @Param(value = "email") String email);
+
 }
