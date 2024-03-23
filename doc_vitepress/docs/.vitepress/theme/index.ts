e6fc91cc35aadde73cecafb845ee3a8b411e6ff6
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-03-23 14:13:30 +0100

    migrate footer to vitepress doc

--- doc_vitepress/docs/.vitepress/theme/index.ts
+++ doc_vitepress/docs/.vitepress/theme/index.ts
@@ -1,11 +1,13 @@
 // .vitepress/theme/index.ts
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import GlobalLayout from "../theme/layouts/GlobalLayout.vue";
 
 import status from "../components/adr/status.vue";
 
 export default {
   extends: DefaultTheme,
+  Layout: GlobalLayout,
   enhanceApp({ app }) {
     // register your custom global components
     app.component("adrStatus", status);
---
+++ doc_vitepress/docs/.vitepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+import VPLink from "vitepress/dist/client/theme-default/components/VPLink.vue";
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #layout-bottom>
+      <div class="VPHOME">
+        <div style="text-align: center">
+          <VPLink href="/imprint/">Contact and Imprint</VPLink>
+          <div>Made with 💛 in Munich</div>
+        </div>
+      </div>
+    </template>
+  </Layout>
+</template>
