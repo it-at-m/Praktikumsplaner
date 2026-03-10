4384014ac3f546e2bce4c618660737ff02cf4525
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2026-03-10 07:41:14 +0100

    test(backend): fix tests after refactoring

--- praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/rest/AbstractTestdataIntegrationTest.java
+++ praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/rest/AbstractTestdataIntegrationTest.java
@@ -6,17 +6,27 @@ import de.muenchen.oss.praktikumsplaner.TestConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static de.muenchen.oss.praktikumsplaner.TestConstants.SPRING_TEST_PROFILE;
 
 @SpringBootTest(classes = MicroServiceApplication.class)
-@ActiveProfiles({ "local", "db-h2", TestConstants.SPRING_NO_SECURITY_PROFILE })
+@ActiveProfiles({ SPRING_TEST_PROFILE, TestConstants.SPRING_NO_SECURITY_PROFILE })
 @AutoConfigureMockMvc
-public class AbstractTestdataIntegrationTest {
+public abstract class AbstractTestdataIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @ServiceConnection
+    @SuppressWarnings("unused")
+    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(
+            DockerImageName.parse(TestConstants.TESTCONTAINERS_POSTGRES_IMAGE));
 }
