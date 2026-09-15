3b3b585beae644ebd40f106c71db41254c9276bd
simonhir <14137977+simonhir@users.noreply.github.com>
2026-09-15 12:46:57 +0200

    chore: implement review feedback

--- praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenServiceTest.java
+++ praktikumsplaner-backend/src/test/java/de/muenchen/oss/praktikumsplaner/service/PraktikumsstellenServiceTest.java
@@ -1,7 +1,6 @@
 package de.muenchen.oss.praktikumsplaner.service;
 
 import static de.muenchen.oss.praktikumsplaner.TestUtils.getJwtAuthenticationToken;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
